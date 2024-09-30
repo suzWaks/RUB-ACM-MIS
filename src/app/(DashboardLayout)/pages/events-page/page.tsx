72764835688bae7bc40db2d7aee0fc517e81b472
peeymaa2020 <peelham2018@gmail.com>
2024-09-30 10:16:33 +0600

    Events

--- src/app/(DashboardLayout)/pages/events-page/page.tsx
+++ src/app/(DashboardLayout)/pages/events-page/page.tsx
@@ -1,13 +1,27 @@
 "use client";
-import { Typography } from "@mui/material";
+
+import React from "react";
+import { Grid } from "@mui/material";
+import Calendar from "./calendar";
+import EventSidebar from "./eventsidebar";
 import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
 import DashboardCard from "@/app/(DashboardLayout)/components/shared/DashboardCard";
 
-const EventsPage = () => {
+const EventsPage: React.FC = () => {
   return (
     <PageContainer title="Event Page" description="Members page">
-      <DashboardCard title="Event Page">
-        <Typography>Modify this page</Typography>
+      <DashboardCard title="Event">
+        <Grid container spacing={3}>
+          {/* Sidebar Section (Left) */}
+          <Grid item xs={3}>
+            <EventSidebar />
+          </Grid>
+
+          {/* Calendar Section (Right) */}
+          <Grid item xs={9}>
+            <Calendar />
+          </Grid>
+        </Grid>
       </DashboardCard>
     </PageContainer>
   );
